af52859595f5cc7218eebd7cdfcc87bbdab9cd9c
APCS3rd <APCS3rd@MHS-LT9-20>
2019-08-20 10:12:12 -0700

    complete Hello World project

---
+++ Hello World Project/src/HelloWorld.java
@@ -0,0 +1,13 @@
+//Anthony Ouyang
+//August 19, 2019
+//3rd Period
+//System.out.println practice
+public class HelloWorld {
+
+	public static void main(String[] args) {
+	System.out.print("Hello World!");
+	System.out.println("Hello World!");
+	
+	}
+
+}
