c676278d9da981ad6368a281f8e557541ae1fd62
APCS3rd <APCS3rd@MHS-LT9-20>
2019-09-03 11:46:42 -0700

    not done

--- Access Calculate/src/DoMath.java
+++ Access Calculate/src/DoMath.java
@@ -1,7 +1,7 @@
 /*Anthony Ouyang
 /* Period 3
 /* August 30, 2019
- * 
+/* 
  */
 
 public class DoMath {
@@ -9,8 +9,12 @@ public class DoMath {
 	public static void main(String[] args) {
 		System.out.println(Calculate.square(2));
 		System.out.println(Calculate.cube(2));
-		System.out.println(Calculate.average())
-		
+		System.out.println(Calculate.average(2, 4));
+		System.out.println(Calculate.average(3,1,2));
+		System.out.println(Calculate.toDegrees(2));
+		System.out.println(Calculate.toRadians(4));
+		System.out.println(Calculate.discriminant(1, 2, 3));
+		System.out.println(Calculate.toImproperFrac(4, 3, 2));
 	}
 
 }
