2e3d3432056e83b9d89e0d8c13cd948846b94e7d
APCS3rd <APCS3rd@MHS-LT9-20>
2019-09-03 10:21:05 -0700

    balabalabala

---
+++ Access Calculate/src/DoMath.java
@@ -0,0 +1,16 @@
+/*Anthony Ouyang
+/* Period 3
+/* August 30, 2019
+ * 
+ */
+
+public class DoMath {
+
+	public static void main(String[] args) {
+		System.out.println(Calculate.square(2));
+		System.out.println(Calculate.cube(2));
+		System.out.println(Calculate.average())
+		
+	}
+
+}
